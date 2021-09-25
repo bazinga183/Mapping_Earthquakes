a988afa03ac5e19fc04a28c5b10ad6a0af059e1e
bazinga183 <eddie.flores183@gmail.com>
2021-09-24 20:06:40 -0500

    Added popups to the map

---
+++ Earthquakes_past7days/static/js/logicStep3.js
@@ -0,0 +1,86 @@
+// Tile layer
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Tile layer
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+    "Street": streets,
+    "Satellite": satelliteStreets
+};
+
+// Style for the map to be created according to the magnitude of the earthquake.
+function styleInfo(feature) {
+    return{
+        opacity: 1,
+        fillOpacity: 1,
+        fillColor: getColor(feature.properties.mag),
+        color: "#000000",
+        radius: getRadius(feature.properties.mag),
+        stroke: true,
+        weight: 0.5
+    };
+};
+
+//This function determines the radius of the earthquake marker based on magnitude.
+function getRadius(magnitude) {
+    if (magnitude === 0) {
+        return 1;
+    }
+    return magnitude * 4;
+};
+
+//This function is to determine the color an earthquake marker is based on magnitude.
+function getColor(magnitude) {
+    if (magnitude > 5) {
+        return "#ea2c2c";
+    } 
+    if (magnitude > 4) {
+        return "#ea822c";
+    }
+    if (magnitude > 3) {
+        return "#ee9c00";
+    }
+    if (magnitude > 2) {
+        return "#d4ee00";
+    }
+    if (magnitude > 1) {
+        return "#d4ee00";
+    }
+    return "#98ee00";
+}
+
+// Create the map object with a center and a zoom level.
+let map = L.map("mapid", {
+    center: [39.5, -98.5],
+    zoom: 3,
+    layers: [streets]
+});
+
+// Add another map layer.
+L.control.layers(baseMaps).addTo(map);
+
+// Grabbing our GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+    console.log(data);
+    // Creating a GeoJSON layer with the retrieved data.
+    L.geoJSON(data, {
+        pointToLayer: function(feature, latlng) {
+            console.log(data);
+            return L.circleMarker(latlng);
+        },
+        style: styleInfo,
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup("Magnitude: " + feature.properties.mag + "<br> Location: " + feature.properties.place)
+        }
+    }).addTo(map);
+});
